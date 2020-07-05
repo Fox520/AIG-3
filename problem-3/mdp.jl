4f2863cdf0f5002687df9bb34b4efd036f531335
Thomas <>
2020-07-05 22:06:31 +0200

    Allow network input

--- problem-3/mdp.jl
+++ problem-3/mdp.jl
@@ -4,29 +4,74 @@ mutable struct State
     transitions::Array{Any,1}
 end
 
-DISCOUNT = 0.5
+function input(prompt::AbstractString="")
+    print(prompt)
+    return parse(Int64, chomp(readline()))
+end
+function input_float(prompt::AbstractString="")
+    print(prompt)
+    return parse(Float64, chomp(readline()))
+end
+function input_special(prompt::AbstractString="")
+    print(prompt)
+    s = split(chomp(readline()), ",")
+    return parse.(Int64, s)
+end
+
+# We wish to model this problem as an MDP and consider a discount value σ. Write a program
+# in Julia that accepts as input the description of the MDP, including its parameters (e.g., reward
+# value for each state, discount and probability distribution for each transition model). The program
+# will return the optimal policy using the value iteration algorithm.
 max_error = 0.001
+DISCOUNT = input_float("Enter discount:")
+all_states = []
+for i in 1:input("Enter number of states: ")
+#     push!(all_states, State(0, 3, []))
+    push!(all_states, State(0, input("Enter reward for state $i: "), []))
+end
+println("----------Collecting transitions--------")
 
-s1 = State(0, 3, [])
-s2 = State(0, -1, [])
-push!(s1.transitions, [s2, 1.0])
-push!(s1.transitions, [s1, 0.5])
-push!(s1.transitions, [s2, 0.5])
+# Display the states with index
+for i in 1:size(all_states)[1]
+    println(i, " => ",all_states[i])
+end
+println("----------------------------------------")
+# Get the transitions
+for i in 1:size(all_states)[1]
+    while true
+        println("Current state => $i")
+        ti = input("Enter index of state to transition: ")
+        p = input_float("Enter probability of transition: ")
+        push!(all_states[i].transitions, [all_states[ti], p])
+        if(input("Add another transition? [1 or 0]") == 0)
+            break
+        end
+    end
+    println()
+end
 
-push!(s2.transitions, [s1, 1.0])
-push!(s2.transitions, [s2, 1.0])
 
-states = [s1, s2]
+
+
+# s1 = State(0, 3, [])
+# s2 = State(0, -1, [])
+
+# push!(s1.transitions, [s2, 1.0])
+# push!(s1.transitions, [s1, 0.5])
+# push!(s1.transitions, [s2, 0.5])
+
+# push!(s2.transitions, [s1, 1.0])
+# push!(s2.transitions, [s2, 1.0])
+
+states = all_states #[s1, s2]
 function get_max_transitions(transitions)
     results = []
     if size(transitions)[1] > 1
         # Just to be sure that not only one transition/action is present
         for i in 2:size(transitions)[1]
-#             println("pushing to result")
             push!(results, transitions[i][2] * transitions[i][1].utility)
         end
     end
-#     println(transitions[1][2] * transitions[1][1].utility, " vs ", sum(results))
     return max(transitions[1][2] * transitions[1][1].utility, sum(results))
 end
 
@@ -62,7 +107,6 @@ while true
         println("Converge ", current_state.utility," ", previous_utility)
         break
     end
-    # utility + reward + beta * max(P*utilityOftransState)
 end
 for i in 1:size(states)[1]
     println(states[i].utility)
